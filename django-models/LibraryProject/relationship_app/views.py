09971e28a9e85e057c589eaad1fbfbcda8493761
hidaya <hidaya.regragui@gmail.com>
2025-08-02 11:08:58 +0100

    html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,10 +1,13 @@
 from django.views.generic.detail import DetailView
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Library
 from .models import Book
+from .models import Author
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+
 
 def list_books(request):
     books = Book.objects.all()
@@ -53,3 +56,53 @@ def librarian_view(request):
 @user_passes_test(is_member)
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
+
+
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    authors = Author.objects.all()
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_id = request.POST.get('author')
+        error = None
+        if title and author_id:
+            try:
+                author = Author.objects.get(id=author_id)
+                Book.objects.create(title=title, author=author)
+                return redirect('list_books')
+            except Author.DoesNotExist:
+                error = "Invalid author selected."
+        else:
+            error = "Please provide both title and author."
+        return render(request, 'relationship_app/add_book.html', {'authors': authors, 'error': error})
+    return render(request, 'relationship_app/add_book.html', {'authors': authors})
+
+@permission_required('relationship_app.can_change_book')
+def edit_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    authors = Author.objects.all()
+    error = None
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        author_id = request.POST.get('author')
+        if title and author_id:
+            try:
+                author = Author.objects.get(id=author_id)
+                book.title = title
+                book.author = author
+                book.save()
+                return redirect('list_books')
+            except Author.DoesNotExist:
+                error = "Invalid author selected."
+        else:
+            error = "Please provide both title and author."
+        return render(request, 'relationship_app/edit_book.html', {'book': book, 'authors': authors, 'error': error})
+    return render(request, 'relationship_app/edit_book.html', {'book': book, 'authors': authors})
+
+@permission_required('relationship_app.can_delete_book')
+def delete_book(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if request.method == 'POST':
+        book.delete()
+        return redirect('list_books')
+    return render(request, 'relationship_app/delete_book.html', {'book': book})
