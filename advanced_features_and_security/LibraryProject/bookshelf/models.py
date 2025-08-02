e3607e8b6bd093355a13ed8a220d75cba34b2a14
hidaya <hidaya.regragui@gmail.com>
2025-08-02 11:47:33 +0100

    task1

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -3,9 +3,20 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
 
 
 class CustomUserManager(BaseUserManager):
