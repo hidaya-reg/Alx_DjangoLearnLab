f4718d43d3f6fac94f04791bbef82da5b49d08c3
hidaya <hidaya.regragui@gmail.com>
2025-08-02 15:55:22 +0100

    task3

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -53,6 +53,9 @@ SECURE_CONTENT_TYPE_NOSNIFF = True
 
 # Enable browser's XSS filter (note: deprecated in some browsers)
 SECURE_BROWSER_XSS_FILTER = True
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Application definition
 
 INSTALLED_APPS = [
